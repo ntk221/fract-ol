08a8b089adb303f0a16b10039a6b42ed239e2c8a
ntk221 <nittakdub@gmail.com>
2023-01-06 09:34:02 +0900

    [Update #1] draw red line

--- learning/intro.c
+++ learning/intro.c
@@ -28,11 +28,8 @@ int	main(void)
   mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello, World!");
   img.img = mlx_new_image(mlx, 1920, 1080);
   img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-  my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-  my_mlx_pixel_put(&img, 5, 6, 0x00FF0000);
-  my_mlx_pixel_put(&img, 5, 7, 0x00FF0000);
-  my_mlx_pixel_put(&img, 5, 8, 0x00FF0000);
-  my_mlx_pixel_put(&img, 5, 9, 0x00FF0000);
+  for(int i = 0; i < 1080; i++)
+    my_mlx_pixel_put(&img, 5, i, 0x00FF0000);
   mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
   mlx_loop(mlx);
 }
