4d1783e139791620b2a08c313adf22336cb78e55
ntk221 <92190938+ntk221@users.noreply.github.com>
2023-01-07 02:45:15 +0000

    [Update #10] defined complex_number type

--- learning/Mandelbrot/membership.c
+++ learning/Mandelbrot/membership.c
@@ -1,11 +1,23 @@
 #include <math.h>
 #include <stdbool.h>
 
-bool    mandelbrot()
+typedef struct s_complex
+{
+    double re;
+    double im;
+} t_complex;
+
+bool    mandelbrot(t_complex c)
+{
+    return true;
+}
 
 void    test_mandelbrot(void)
 {
-    bool res = mandelbrot(t_complex c);
+    t_complex   c;
+    c.re = 1.0;
+    c.im = 1.0;
+    bool res = mandelbrot(c);
 }
 
 int main(void)
