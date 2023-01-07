ecb1f1bd649cd790e5aefca4c2b58ce878371bf3
ntk221 <92190938+ntk221@users.noreply.github.com>
2023-01-07 02:35:19 +0000

    [Update #7] found another problem

---
+++ learning/Mandelbrot/membership.c
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <stdbool.h>
+
+bool    mandelbrot()
+
+void    test_mandelbrot(void)
+{
+    bool res = mandelbrot(t_complex c);
+}
+
+int main(void)
+{
+    test_mandelbrot();
+}
